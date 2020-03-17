b698bdc78dd23da0e4233977c50e9570864fe9e0
Pawlost <mr.p.balusek@gmail.com>
2020-03-17 19:32:56 +0100

    Optimized build

--- FastNoiseSIMD-wrapper.h
+++ FastNoiseSIMD-wrapper.h
@@ -1,19 +1,19 @@
-#pragma unmanaged
+#include "FastNoiseSIMD/FastNoiseSIMD.h"
+#pragma managed
 
 using namespace System;
-#include "FastNoiseSIMD/FastNoiseSIMD/FastNoiseSIMD.h"
 namespace CLI {
 	public ref class FastNoiseSIMDwrapper
 	{
-		FastNoiseSIMD* fastnoise;
-
 	public:
-		FastNoiseSIMDwrapper(int seed) { fastnoise = FastNoiseSIMD::NewFastNoiseSIMD(seed); }
+		FastNoiseSIMDwrapper(int seed) : fastnoise(FastNoiseSIMD::NewFastNoiseSIMD(seed)) {  }
 			~FastNoiseSIMDwrapper() { this->!FastNoiseSIMDwrapper(); }
 			!FastNoiseSIMDwrapper() { delete fastnoise; }
 
-	float GetPerlinFractalSet(int startx, int starty, int startz, int sizex, int sizey, int sizez, float frequency) {
+		float GetPerlinFractalSet(int startx, int starty, int startz, int sizex, int sizey, int sizez, float frequency) {
 		return *fastnoise->GetPerlinFractalSet(startx, starty, startz, sizex, sizey, sizez, frequency);
-	}
+		}
+	private:
+		FastNoiseSIMD* fastnoise;
 	};
 }
